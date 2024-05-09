7027df039fcd06ae4e5ca240b52222082a6f2ba8
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-05-09 22:46:46 +0600

    using daisy ui for this project

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ function App() {
   return (
     <>
       <h1>Ashifur rahaman</h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-// import formsPlugin from '@tailwindcss/forms'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('daisyui'),
+  ],
+}
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui")],
-// }
